68c118aaa474a10bf173eded3a2b377157dc0903
shital bramhe <shitalbramhe@gmail.com>
2021-11-29 20:47:37 +0530

    [shital] ADD: validate Mobile Number

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -10,7 +10,7 @@ namespace UserRegistration
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Validate last name \n3. Exit");
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Validate last name \n3. Validate EmailID \n4. Validate Mobile No.  \n5. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -31,6 +31,18 @@ namespace UserRegistration
                         lnamepattern.Checklastname(lname);
                         break;
                     case 3:
+                        Console.WriteLine("Enter Email id :");
+                        string email = Console.ReadLine();
+                        Validation_regex emailpattern = new Validation_regex();
+                        emailpattern.Checkemail(email);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Mobile No. :");
+                        string mobilenum = Console.ReadLine();
+                        Validation_regex mobilenumpattern = new Validation_regex();
+                        mobilenumpattern.Checkmobilenum(mobilenum);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                     default:
--- UserRegistration/UserRegistration/Validation_regex.cs
+++ UserRegistration/UserRegistration/Validation_regex.cs
@@ -25,6 +25,22 @@ namespace UserRegistration
             else
                 Console.WriteLine("Invalid name!!");
         }
+        public void Checkemail(string Emailid)
+        {
+            string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            if (Regex.IsMatch(Emailid, emailpattern))
+                Console.WriteLine("Email id is valid");
+            else
+                Console.WriteLine("Invalid email id!!");
+        }
+        public void Checkmobilenum(string Mobilenum)
+        {
+            string mobilenumpattern = "^[1,9]{2}-[6-9]{1}[0-9]{9}";
+            if (Regex.IsMatch(Mobilenum, mobilenumpattern))
+                Console.WriteLine("Mobile number is valid");
+            else
+                Console.WriteLine("Invalid mobile number!!");
+        }
     }
 }
     
