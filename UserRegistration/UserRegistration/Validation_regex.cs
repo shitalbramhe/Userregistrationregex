850586ad08f30f91b462b4c994b79a34d2d63276
shital bramhe <shitalbramhe@gmail.com>
2021-12-07 16:50:22 +0530

    [shital] ADD: clear all email sample

--- UserRegistration/UserRegistration/Validation_regex.cs
+++ UserRegistration/UserRegistration/Validation_regex.cs
@@ -27,13 +27,12 @@ namespace UserRegistration
         }
         public void Checkemail(string Emailid)
         {
-            string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            //string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            string emailpattern = "^[a-zA-Z0-9]+[._+-]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
             if (Regex.IsMatch(Emailid, emailpattern))
                 Console.WriteLine("Email id is valid");
             else
-                Console.WriteLine("Invalid name!!");
-
-            Console.WriteLine("Invalid email id!!");
+                Console.WriteLine("Invalid Email id!!");
         }
         public void Checkmobilenum(string Mobilenum)
         {
